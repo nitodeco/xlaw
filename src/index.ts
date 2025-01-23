573f0591e4ca381be95163e0f5d472ce3cb74b54
nitodeco <nitodeco@users.noreply.github.com>
2025-01-23 15:34:26 +0100

    fix missing default export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import * as alaw from "./lib/alaw";
 import * as mulaw from "./lib/mulaw";
 import * as utils from "./lib/utils";
 
-export { alaw, mulaw, utils };
+export default { alaw, mulaw, utils };
